6b4f4fdc9a9c22c8c64f02911f0da8e84c15e517
gijsvandenbeuken <g.vandenbeuken@student.fontys.nl>
2023-12-06 10:59:48 +0100

    Test cicd

--- test/authController.test.js
+++ test/authController.test.js
@@ -7,33 +7,21 @@ chai.use(chaiHttp);
 
 describe("Auth Controller", () => {
   describe("handleLogin", () => {
-    it("should handle POST /auth with valid credentials", (done) => {
-      chai
-        .request(app)
-        .post("/auth")
-        .send({
-          user: "Jake",
-          pwd: "Abc123@@@",
-        })
-        .end((err, res) => {
-          expect(res).to.have.status(200);
-          expect(res.body.roles).to.be.an("array").that.includes(2001);
-          done();
-        });
+    it("should handle POST /auth with valid credentials", async () => {
+      const res = await chai.request(app).post("/auth").send({
+        user: "Jake",
+        pwd: "Abc123@@@",
+      });
+      expect(res).to.have.status(200);
+      expect(res.body.roles).to.be.an("array").that.includes(2001);
     });
 
-    it("should handle POST /auth with invalid credentials", (done) => {
-      chai
-        .request(app)
-        .post("/login")
-        .send({
-          user: "invalidUser",
-          pwd: "invalidPassword",
-        })
-        .end((err, res) => {
-          expect(res).to.have.status(401);
-          done();
-        });
+    it("should handle POST /auth with invalid credentials", async () => {
+      const res = await chai.request(app).post("/login").send({
+        user: "invalidUser",
+        pwd: "invalidPassword",
+      });
+      expect(res).to.have.status(401);
     });
   });
 });
